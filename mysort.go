bdd91b44c8e4c88e9248941ccbfff6326989c404
Sun Wang <sun.wxg@gmail.com>
2015-12-17 15:12:17 +0800

    init start

---
+++ mysort.go
@@ -0,0 +1,140 @@
+package mysort
+
+import "reflect"
+
+type Compare func(a, b interface{}) bool
+
+func swapValue(a, b reflect.Value) {
+	t := a.Interface()
+	a.Set(b)
+	b.Set(reflect.ValueOf(t))
+}
+
+func InsertSort(l interface{}, f Compare) {
+
+	list := reflect.ValueOf(l)
+	if list.Kind() != reflect.Slice {
+		return
+	}
+
+	for i := 1; i < list.Len(); i++ {
+		for k := i; k > 0; k-- {
+			a := list.Index(k - 1).Interface()
+			b := list.Index(k).Interface()
+			if f(a, b) {
+				break
+			} else {
+				swapValue(list.Index(k-1), list.Index(k))
+			}
+		}
+	}
+}
+
+func selectIndex(list reflect.Value, left, right int) int {
+	return left
+}
+
+func partition(list reflect.Value, left, right int, f Compare) int {
+
+	index := selectIndex(list, left, right)
+
+	storeIndex := left
+
+	swapValue(list.Index(index), list.Index(right))
+
+	b := list.Index(right).Interface()
+	for i := left; i < right; i++ {
+		a := list.Index(i).Interface()
+		if f(a, b) {
+			swapValue(list.Index(i), list.Index(storeIndex))
+			storeIndex++
+		}
+	}
+
+	swapValue(list.Index(right), list.Index(storeIndex))
+	return storeIndex
+}
+
+func qSort(list reflect.Value, left, right int, f Compare) {
+
+	if left >= right {
+		return
+	}
+
+	index := partition(list, left, right, f)
+
+	qSort(list, left, index-1, f)
+	qSort(list, index+1, right, f)
+}
+
+func QuickSort(l interface{}, f Compare) {
+	list := reflect.ValueOf(l)
+	if list.Kind() != reflect.Slice {
+		return
+	}
+
+	qSort(list, 0, list.Len()-1, f)
+}
+
+func shiftUp(list reflect.Value, i int, f Compare) {
+
+	if i == 0 {
+		return
+	}
+
+	iParent := (i - 1) / 2
+
+	a := list.Index(iParent).Interface()
+	b := list.Index(i).Interface()
+	if f(a, b) {
+		swapValue(list.Index(iParent), list.Index(i))
+		shiftUp(list, iParent, f)
+	}
+}
+
+func buildHeap(list reflect.Value, f Compare) {
+	for i := 1; i < list.Len(); i++ {
+		shiftUp(list, i, f)
+	}
+}
+
+func shiftDown(list reflect.Value, i, iMax int, f Compare) {
+
+	iLeft := 2*i + 1
+	if iLeft > iMax {
+		return
+	} else {
+		a := list.Index(i).Interface()
+		b := list.Index(iLeft).Interface()
+		if f(a, b) {
+			swapValue(list.Index(iLeft), list.Index(i))
+			shiftDown(list, iLeft, iMax, f)
+		}
+	}
+
+	iRight := 2*i + 2
+	if iRight > iMax {
+		return
+	} else {
+		a := list.Index(i).Interface()
+		b := list.Index(iRight).Interface()
+		if f(a, b) {
+			swapValue(list.Index(iRight), list.Index(i))
+			shiftDown(list, iRight, iMax, f)
+		}
+	}
+}
+
+func HeapSort(l interface{}, f Compare) {
+	list := reflect.ValueOf(l)
+	if list.Kind() != reflect.Slice {
+		return
+	}
+
+	buildHeap(list, f)
+
+	for i := list.Len() - 1; i > 0; i-- {
+		swapValue(list.Index(0), list.Index(i))
+		shiftDown(list, 0, i-1, f)
+	}
+}
---
+++ mysort_test.go
@@ -0,0 +1,177 @@
+package mysort_test
+
+import (
+	"fmt"
+	"math/rand"
+	"mypkg/mysort"
+	"testing"
+)
+
+//a > b = ture
+func c(a, b interface{}) bool {
+	av := a.(int)
+	bv := b.(int)
+
+	switch {
+	case av > bv:
+		return true
+	case av <= bv:
+		return false
+	}
+	return false
+}
+
+//a < b = ture
+func u(a, b interface{}) bool {
+	av := a.(int)
+	bv := b.(int)
+
+	switch {
+	case av < bv:
+		return true
+	case av >= bv:
+		return false
+	}
+	return false
+}
+
+func TestInsertSort(t *testing.T) {
+	list := []int{1, 2, 3, 6, 5}
+	mysort.InsertSort(list, c)
+
+	result := list
+	wanted := []int{6, 5, 3, 2, 1}
+
+	for i, v := range wanted {
+		if v != result[i] {
+			t.Fatalf("InsertSort: wanted (%v), geted (%v)", wanted, result)
+			break
+		}
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	list := []int{1, 2, 3, 6, 5}
+	mysort.QuickSort(list, c)
+
+	result := list
+	wanted := []int{6, 5, 3, 2, 1}
+
+	for i, v := range wanted {
+		if v != result[i] {
+			t.Fatalf("QuickSort: wanted (%v), geted (%v)", wanted, result)
+			break
+		}
+	}
+}
+
+func TestHeapSortDown(t *testing.T) {
+	list := []int{1, 2, 3, 6, 5}
+	mysort.HeapSort(list, c)
+
+	result := list
+	wanted := []int{6, 5, 3, 2, 1}
+
+	for i, v := range wanted {
+		if v != result[i] {
+			t.Fatalf("HeapSort: wanted (%v), geted (%v)", wanted, result)
+			break
+		}
+	}
+}
+
+func TestHeapSortUp(t *testing.T) {
+	list := []int{9, 2, 3, 6, 5}
+	mysort.HeapSort(list, u)
+
+	result := list
+	wanted := []int{2, 3, 5, 6, 9}
+
+	for i, v := range wanted {
+		if v != result[i] {
+			t.Fatalf("HeapSort: wanted (%v), geted (%v)", wanted, result)
+			break
+		}
+	}
+}
+
+func TestDown(t *testing.T) {
+	r := rand.New(rand.NewSource(99))
+	var l []int
+	for i := 0; i < 100; i++ {
+		l = append(l, r.Intn(1000))
+	}
+
+	listInsert := make([]int, 100)
+	for i, _ := range listInsert {
+		listInsert[i] = l[i]
+	}
+	mysort.InsertSort(listInsert, c)
+
+	listQuickSort := make([]int, 100)
+	for i, _ := range listQuickSort {
+		listQuickSort[i] = l[i]
+	}
+	mysort.QuickSort(listQuickSort, c)
+
+	listHeapSort := make([]int, 100)
+	for i, _ := range listHeapSort {
+		listHeapSort[i] = l[i]
+	}
+	mysort.HeapSort(listHeapSort, c)
+
+	for i, v := range listInsert {
+		if v != listQuickSort[i] {
+			t.Fatalf("HeapSort don't equal QuickSort")
+			break
+		}
+	}
+
+	fmt.Println(listInsert)
+	for i, v := range listInsert {
+		if v != listHeapSort[i] {
+			t.Fatalf("HeapSort don't equal HeapSort")
+			break
+		}
+	}
+}
+
+func TestUp(t *testing.T) {
+	r := rand.New(rand.NewSource(99))
+	var l []int
+	for i := 0; i < 100; i++ {
+		l = append(l, r.Intn(1000))
+	}
+
+	listInsert := make([]int, 100)
+	for i, _ := range listInsert {
+		listInsert[i] = l[i]
+	}
+	mysort.InsertSort(listInsert, u)
+
+	listQuickSort := make([]int, 100)
+	for i, _ := range listQuickSort {
+		listQuickSort[i] = l[i]
+	}
+	mysort.QuickSort(listQuickSort, u)
+
+	listHeapSort := make([]int, 100)
+	for i, _ := range listHeapSort {
+		listHeapSort[i] = l[i]
+	}
+	mysort.HeapSort(listHeapSort, u)
+
+	for i, v := range listInsert {
+		if v != listQuickSort[i] {
+			t.Fatalf("HeapSort don't equal QuickSort")
+			break
+		}
+	}
+
+	for i, v := range listInsert {
+		if v != listHeapSort[i] {
+			t.Fatalf("HeapSort don't equal HeapSort")
+			break
+		}
+	}
+}
